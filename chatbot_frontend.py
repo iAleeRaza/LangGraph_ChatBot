a56e8f500ebc662f298d6af3a9009806afff4282
Alee Raza <arm88002255@gmail.com>
2026-05-15 17:25:30 +0500

    initial_commit

---
+++ chatbot_frontend.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+# loading the conversation history
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+user_input = st.chat_input("Type Here...")
+
+if user_input:
+
+    st.session_state["message_history"].append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+   
+    st.session_state["message_history"].append({"role": "assistant", "content": user_input})
+    with st.chat_message("assistant"):
+        st.text(user_input)
